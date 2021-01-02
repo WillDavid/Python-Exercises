54fd3ce06b96ff5000260086a6d105c4d88be6a9
William David Martins de Almeida <wdma.lic17@uea.edu.br>
2021-01-02 13:29:06 -0300

    update ap02

--- Freelas/Vinicius/Ap02/ap02.py
+++ Freelas/Vinicius/Ap02/ap02.py
@@ -3,11 +3,19 @@ Ap 02
 
 '''
 
+def ordenar(listaNomes): 
+    tamanhoLista = len(listaNomes) 
+    for i in range(tamanhoLista-1): 
+        for j in range(0, tamanhoLista-i-1): 
+            if listaNomes[j] > listaNomes[j+1] : 
+                listaNomes[j], listaNomes[j+1] = listaNomes[j+1], listaNomes[j] 
+
 qtdAlunos = int(input())
 nomesAlunos = []
 nota01 = []
 nota02 = []
 mediaAluno = []
+dicionario = {}
 
 for i in range(qtdAlunos):
   nomesAlunos.append(input())
@@ -16,9 +24,20 @@ for i in range(qtdAlunos):
 
   media = (nota01[i] + nota02[i])/2
   mediaAluno.append(media)
-  
 
-  
+
+for i in range(len(nomesAlunos)):
+  dicionario.update({nomesAlunos[i]: mediaAluno[i]})
+
+
+ordenar(nomesAlunos)
+
+for i in range(len(nomesAlunos)): 
+    print (nomesAlunos[i])
+    print(dicionario[nomesAlunos[i]])
+
+
+
 
 
 
